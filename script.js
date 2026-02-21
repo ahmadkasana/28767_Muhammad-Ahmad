cd8d63fcac908581857dc51b9ab0077152985c50
Muhammad Ahmad Gujjar <ahmadkasana514@gmail.com>
2026-02-21 22:41:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Simple Portfolio JavaScript
+ * Uses basic concepts: variables, functions, click events
+ */
+
+// Toggle the mobile menu when Menu button is clicked
+function toggleMenu() {
+  var navLinks = document.getElementById("navLinks");
+  navLinks.classList.toggle("show");
+}
